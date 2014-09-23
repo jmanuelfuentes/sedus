4d92fa4f0d642e2636f64486a3039e67f9c2ca94
jmanuelfuentes <jmanuel.fuentes@upf.edu>
2014-09-23 09:11:38 +0200

    splash, ui changes

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QSplashScreen>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QPixmap pixmap("./img/splash.png");
+    QSplashScreen splash(pixmap);
+    splash.show();
+    splash.showMessage(QObject::tr("Initiating SEDUS..."),
+                        Qt::AlignLeft | Qt::AlignTop, Qt::black);
+
+    a.processEvents();
+    QThread::sleep(5);
     MainWindow w;
     w.show();
-
+    splash.finish(&w);
+    splash.raise();
     return a.exec();
 }
